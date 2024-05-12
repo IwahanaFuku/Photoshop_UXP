83eb643d3ddf5bfb6b251345d59bd2f037d5356b
IwahanaFuku <mutsuki.iwahana@gmail.com>
2024-05-12 16:15:27 +0900

    全て書き出し機能

--- Test-18dzoe/javascript/main.js
+++ Test-18dzoe/javascript/main.js
@@ -3,114 +3,101 @@ const { app } = require("photoshop");
 const lffs = storage.localFileSystem;
 const fs = require('fs');
 
-showAlert = () => {
-  alert("This is an alert message");
-}
+class InputSetManager {
 
-entrypoints.setup({
-  commands: {
-    showAlert,
-  },
-  panels: {
-    vanilla: {
-      show(node ) {
-      }
-    }
-  }
-});
-
-function addNewInputSet() {
-
-  const folderNameInput = textInputElement("folder path");
-  const fileNameInput = textInputElement("file name");
-  const selectFolderButton = buttonElement("フォルダ選択");
-  const selectBox = selectBoxElement();
-  const exportButton = buttonElement("書き出し");
-
-  const div = document.createElement("div");
-
-  div.appendChild(folderNameInput);
-  div.appendChild(fileNameInput);
-  div.appendChild(selectBox);
-  div.appendChild(selectFolderButton);
-  div.appendChild(exportButton);
-
-  selectFolderButton.addEventListener("click", () => {
-    folderName = selectFolder(folderNameInput);
-  });
-
-  exportButton.addEventListener("click", () => {
-
-    let fileFullPath = path.join(folderNameInput.value, fileNameInput.value + selectBox.value);
-    console.log(fileFullPath);
-
-    FileExporter.exportFile(fileFullPath);
-  });
-  
-  const container = document.querySelector("#container");
-  container.appendChild(div);
-}
-
-function textInputElement(placeholder)
-{
-  const input = document.createElement("input");
-  input.type = "text";
-  input.placeholder = placeholder;
-
-  return input;
-}
-
-function buttonElement(textContent)
-{
-  const button = document.createElement("button");
-  button.textContent = textContent;
-
-  return button;
-}
-
-async function selectFolder(pathInput)
-{
-  let folderName;
-
-  try
-  {
-      const folder = await lffs.getFolder();
-      if (folder) {
-          console.log("Selected folder: " + folder.nativePath);
-          folderName = folder.nativePath;
-      }
-  } catch (error) {
-      console.error("Error selecting folder:", error);
+  constructor(containerId) {
+      this.container = document.querySelector(containerId);
+      this.initializeEventListeners();
   }
 
-  pathInput.value = folderName;
+  addNewInputSet() {
+      const div = document.createElement("div");
+      div.classList.add("input-set");
+      div.appendChild(this.textInputElement("folder path"));
+      div.appendChild(this.textInputElement("file name"));
+      div.appendChild(this.selectBoxElement());
 
-  return folderName;
+      const selectFolderButton = this.buttonElement("フォルダ選択");
+      const exportButton = this.buttonElement("書き出し");
+      const deleteButton = this.buttonElement("削除");
+      const hiddenPathElement = document.createElement("p");
+      div.appendChild(selectFolderButton);
+      div.appendChild(exportButton);
+      div.appendChild(deleteButton);
+      div.appendChild(hiddenPathElement);
+
+      this.setUpEventListeners(div, selectFolderButton, exportButton, deleteButton, hiddenPathElement);
+      this.container.appendChild(div);
+  }
+
+  textInputElement(placeholder) {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.placeholder = placeholder;
+      return input;
+  }
+
+  buttonElement(textContent) {
+      const button = document.createElement("button");
+      button.textContent = textContent;
+      return button;
+  }
+
+  selectBoxElement() {
+      const select = document.createElement('select');
+      const options = FileExporter.getSupportedExtensions();
+      options.forEach(option => {
+          const newOption = document.createElement('option');
+          newOption.value = option;
+          newOption.textContent = option;
+          select.appendChild(newOption);
+      });
+      select.value = options[0];
+      return select;
+  }
+
+  setUpEventListeners(div, selectFolderButton, exportButton, deleteButton, hiddenPathElement) {
+      selectFolderButton.addEventListener("click", async () => {
+          const folderName = await lffs.getFolder();
+          if (folderName) {
+              div.querySelector("[placeholder='folder path']").value = folderName.nativePath;
+              this.updateFullPath(div, hiddenPathElement);
+          }
+      });
+
+      div.querySelectorAll("input, select").forEach(input => {
+          input.addEventListener("input", () => this.updateFullPath(div, hiddenPathElement));
+          input.addEventListener("change", () => this.updateFullPath(div, hiddenPathElement));
+      });
+
+      exportButton.addEventListener("click", () => FileExporter.exportFile(hiddenPathElement.textContent));
+      deleteButton.addEventListener("click", () => div.remove());
+  }
+
+  updateFullPath(div, hiddenPathElement) {
+      const folderNameInput = div.querySelector("[placeholder='folder path']");
+      const fileNameInput = div.querySelector("[placeholder='file name']");
+      const selectBox = div.querySelector("select");
+      const fileFullPath = path.join(folderNameInput.value, fileNameInput.value + selectBox.value);
+      hiddenPathElement.textContent = fileFullPath;
+  }
+
+  initializeEventListeners() {
+      document.getElementById("addButton").addEventListener("click", () => this.addNewInputSet());
+      document.getElementById("exportAllButton").addEventListener("click", () => this.exportAll());
+  }
+
+  exportAll() {
+      const inputSets = this.container.querySelectorAll(".input-set");
+      inputSets.forEach(div => {
+          const hiddenPathElement = div.querySelector("p");
+          if (hiddenPathElement && hiddenPathElement.textContent) {
+              console.log("Exporting file at path: ", hiddenPathElement.textContent);
+              FileExporter.exportFile(hiddenPathElement.textContent);
+          }
+      });
+  }
 }
 
-function selectBoxElement() {
-  // 新しいselect要素を作成
-  const select = document.createElement('select');
-  
-  // Option要素を作成し、selectに追加
-  const options = FileExporter.getSupportedExtensions();
-
-  options.forEach(option => {
-    const newOption = document.createElement('option');
-    newOption.value = option;
-    newOption.textContent = option;
-    select.appendChild(newOption);
-  });
-
-  // select要素の初期値を設定
-  select.value = options[0];
-
-  // select要素にイベントリスナーを設定
-  select.addEventListener('change', () => {
-    console.log("Selected Item:", select.value);
-  });
-
-  return select;
-}
-
-document.getElementById("addButton").addEventListener("click", addNewInputSet);
+// Initialize InputSetManager
+const inputSetManager = new InputSetManager("#container");
