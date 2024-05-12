63cbc771293d109478b29937a7b18318eb29b771
IwahanaFuku <mutsuki.iwahana@gmail.com>
2024-05-12 22:45:48 +0900

    画像を統合するbatchplayを実装してみる

--- Test-18dzoe/javascript/alphachannel-to-main.js
+++ Test-18dzoe/javascript/alphachannel-to-main.js
@@ -1,31 +1,18 @@
 class AlphachannelToMain {
-  static app = require("photoshop").app;
-  static core = require("photoshop").core;
 
-  static async AddRedFile() {
-    console.log("test");
+  static async alphachannelToMain() {
 
-    const doc = app.activeDocument;
-    const originalActiveLayer = doc.activeLayers[0];
+    const runModalFunction = async () => {
+      const psAction = require("photoshop").action;
+    
+      await require("photoshop").core.executeAsModal(async () => {
+        const command = [
+          {"_obj": "flattenImage"}
+        ];
+        await psAction.batchPlay(command, {});
+      }, {"commandName": "Action Commands"});
+    };
 
-    // 新しいレイヤーを作成
-    const redLayer = await doc.createLayer({ name: "Red Fill" });
-
-    // 新しいレイヤーをアクティブにする
-    doc.activeLayers = [redLayer];
-
-    // レイヤーを赤色で塗りつぶす
-    await core.executeAsModal(() => {
-        return app.batchPlay(
-            [{
-                _obj: "fill",
-                using: { _enum: "color", _value: "red" },
-                color: { _obj: "RGBColor", red: 255, green: 0, blue: 0 },
-                opacity: 100,
-                mode: { _enum: "blendMode", _value: "normal" }
-            }],
-            { synchronousExecution: false }
-        );
-    });
+    await runModalFunction();
   }
 }
